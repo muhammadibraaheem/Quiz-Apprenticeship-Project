9df61dc49bd0d6e028a69c202654ca1163d12f37
muhammadibraaheem <46523489+muhammadibraaheem@users.noreply.github.com>
2021-01-29 15:49:51 +0000

    receive JSON from post request to API

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # play-project
 
 
+Next: POST API
--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -6,8 +6,13 @@ import play.api.libs.json._
 
 class ApiController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  def apiCall: Action[AnyContent] = Action { implicit request =>
-    val json: JsValue = Json.parse("""{"hello":"world"}""")
+  def getApiCall: Action[AnyContent] = Action { implicit request =>
+    val json: JsValue = Json.parse("""{"hello":"get"}""")
+    Ok(json)
+  }
+
+  def postApiCall: Action[AnyContent] = Action { implicit request =>
+    val json: JsValue = Json.parse("""{"hello":"post"}""")
     Ok(json)
   }
 }
