bb138f095cde8e0cdbca4661aa1fcf8343c5ee2f
muhammadibraaheem <46523489+muhammadibraaheem@users.noreply.github.com>
2021-12-05 00:09:21 +0000

    streamline path

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -22,13 +22,6 @@ class ApiController @Inject()(val controllerComponents: ControllerComponents) ex
     Ok(json)
   }
 
-  def putApiCall: Action[AnyContent] = Action { implicit request =>
-    val json: JsValue = Json.parse("""{"hello":"put"}""")
-    Ok(json)
-  }
 
-  def deleteApiCall: Action[AnyContent] = Action { implicit request =>
-    val json: JsValue = Json.parse("""{"hello":"delete"}""")
-    Ok(json)
-  }
+
 }
